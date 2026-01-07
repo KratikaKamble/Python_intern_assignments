6e18156b69a9b9586dda1c8afbfebc79e7879382
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-07 18:51:49 +0530

    Implementing  basic login check functionality

---
+++ login_check.py
@@ -0,0 +1,6 @@
+username=input("Enter your username:")
+password=input("Enter your password:")
+if username=="dd_admin" and password=="dd@2025":
+    print("Login successful")
+else:
+    print("Access denied:Username and password is incorrect")
