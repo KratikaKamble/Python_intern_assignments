e8963d67a3a48eef0836c78b70cfeab9d9207c08
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-09 16:32:35 +0530

    List_topic.py for list operations example
    Demonstrate list creation and manipulation in Python.

---
+++ List_topic.py
@@ -0,0 +1,26 @@
+#creating lisst
+fruits=["mango","pineapple","apple","dragon","orange"]
+print(fruits)
+#accessing of list
+print(fruits[1])
+print(fruits[-1])
+print(fruits[1:4])
+print(fruits[-5:-2])
+print(fruits[1:4:2])
+#methods of list
+fruits.append("melon")
+print(fruits)
+fruits.insert(1,"starfruit")
+print(fruits)
+fruits.remove("pineapple")
+print(fruits)
+#fruits.pop()
+#print(fruits)
+#sort
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+print(len(fruits))
+#del fruits
+#print(fruits)
