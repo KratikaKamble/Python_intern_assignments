dc44333fbb53d9e291391c7f6444bfc624cd0e4f
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-09 11:11:43 +0530

    Membership operator examples for students and products

---
+++ membership_operator.py
@@ -0,0 +1,9 @@
+#student to be searched
+stud=["chinnu","tanu","jeetu","ammu","krishu","pari","kushi","appu"]
+search=input("Enter the name of students to be searched:\n")
+print(search in stud)
+
+#product verify
+inventory=["laptop","mobile","adaptor","cable"]
+search=input("Enter the name of product to be searched:\n")
+print(search in inventory)
