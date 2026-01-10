dbe3f1f9d435c58e04ad52573bb6d9ed1578c8b8
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-10 15:04:18 +0530

    product category tracker with sample data

---
+++ Product Category Tracker.py
@@ -0,0 +1,6 @@
+prod_cate={"Laptop": "Electronics", "Chair": "Furniture", "Mobile": "Electronics"}
+print("products are=",prod_cate.keys())
+print("Categories are=",prod_cate.values())
+print("Total products=",len(prod_cate.keys()))
+#total=len(prod_cate.keys())
+#print(total)
