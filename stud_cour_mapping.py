66455b50329c0d1099f5b280c6dfa4360c01b322
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-10 16:03:24 +0530

    student-course mapping and print statements

---
+++ stud_cour_mapping.py
@@ -0,0 +1,5 @@
+stud_cour={"Asha": "Python", "Ravi": "Data Analytics", "Neha": "AI"}
+print("Student Names are:",stud_cour.keys())
+print("Enrolled Courses are:",stud_cour.values())
+print("Asha" in stud_cour.keys())
+
