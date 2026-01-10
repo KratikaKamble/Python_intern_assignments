5f612a85a196e127434454cb726707fbabe25143
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-10 14:57:05 +0530

    Item price list calculation

---
+++ E-commerce_item_price_list.py
@@ -0,0 +1,5 @@
+item=[200,500,900,600,800]
+total_order_value=sum(item)
+avg_item_price=total_order_value/len(item)
+print("Total order value=",total_order_value)
+print("Average item price=",avg_item_price)
