54941a7ab760711b1e2a211568c2908850313aaf
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-12 16:12:59 +0530

    Add program to find the largest of three numbers

---
+++ largest_of-3_num.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+if a>b:
+    if a>c:
+        print("a is largest")
+    else:
+        print("c is largest")
+else:
+    if b>c:
+        print("b is largest")
+    else:
+        print("c is largest")
