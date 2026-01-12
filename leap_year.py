7a99cd176e8892ac29700611be70a980a3013ab5
KratikaKamble <kratitanukamble691@gmail.com>
2026-01-12 20:40:19 +0530

    Add leap year checker in leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,5 @@
+year=int(input("Enter the year,you want to know whether the the year is leap year or not:"))
+if year%4==0:
+    print(f"The given year {year} is leap year!!")
+else:
+    print(f"The year {year} is not a leap year")
